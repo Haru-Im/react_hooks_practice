907ae05a4ae1835f181cb8d31e0d95c3cd85029e
Haru Im <chalk323@naver.com>
2023-06-21 15:42:18 +0900

    usememo (1) : 함수 결과값 memoization

--- src/components/UseMemo1.tsx
+++ src/components/UseMemo1.tsx
@@ -1,13 +1,45 @@
-export const UseMemo1 = () => {
-  return <div>d</div>;
+import { ChangeEvent, useMemo, useState } from "react";
+
+// delay 만들기 위해 CPU 많이 사용하는 함수 생성
+const hardCalculator = (n: number) => {
+  console.log("어려운 계산 🥵");
+  for (let i = 0; i < 999999999; i++) {}
+  return n + 10000;
 };
 
-function Component() {
-  const value = calculate();
+const easyCalculator = (n: number) => {
+  console.log("쉬운 계산 😄");
+  return n + 1;
+};
 
-  return <div>{value}</div>;
-}
+export const UseMemo1: React.FC = () => {
+  const [hardNumber, setHardNumber] = useState<number>(0);
+  const [easyNumber, setEasyNumber] = useState<number>(0);
 
-function calculate() {
-  return 1;
-}
+  const handleHDChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setHardNumber(Number(event.target.value));
+  };
+
+  const handleEZChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEasyNumber(Number(event.target.value));
+  };
+
+  const hardSum = useMemo<number>(() => {
+    return hardCalculator(hardNumber);
+  }, [hardNumber]);
+
+  return (
+    <div>
+      <div>
+        <h1>짱 어려운 계산기</h1>
+        <input value={hardNumber} type="number" onChange={handleHDChange} />
+        <span>+10000 = {hardSum}</span>
+      </div>
+      <div>
+        <h1>짱 쉬운 계산기</h1>
+        <input value={easyNumber} type="number" onChange={handleEZChange} />
+        <span>+1 = {easyCalculator(easyNumber)}</span>
+      </div>
+    </div>
+  );
+};
